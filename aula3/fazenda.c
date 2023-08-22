cb3fca3c8f8811f076c15c2504ee212f3cf012fc
Lucas S. Silva <57202598+Coffee-for-Cats@users.noreply.github.com>
2023-08-22 14:46:16 -0300

    aula 3

---
+++ aula3/fazenda.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+// #include <vcruntime.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int limoeiros, goiabeiras;
+  printf("Quantos limoeiros voce possui na sua fazenda? \n");
+  scanf("%d", &limoeiros);
+  printf("Quantas goiabeiras voce possui na sua fazenda? \n");
+  scanf("%d", &goiabeiras);
+
+  printf("Voce vai precisar de %d litros de agua.", (limoeiros * 2) + (goiabeiras * 3));
+}
---
+++ aula3/galinhas.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+// #include <vcruntime.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int galinhas;
+  printf("Quantas galinhas voce possui? \n");
+  scanf("%d", &galinhas);
+
+  float preco;
+  printf("Qual o preco dos ovos hoje? \n");
+  scanf("%f", &preco);
+
+  printf("Hoje voce ganhara R$ %.2f! \n", preco * galinhas);
+}
